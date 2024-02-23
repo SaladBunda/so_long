86e4d84cf2d90c5053569de9eb0155bbe76fbc50
Ahmed El Maazouzi <ael-maaz@c1r7p1.1337.ma>
2024-02-23 16:57:02 +0100

    started workign on events

---
+++ srcs/key_codes.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_codes.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maaz <ael-maaz@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 16:26:05 by ael-maaz          #+#    #+#             */
+/*   Updated: 2024/02/23 16:29:27 by ael-maaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEY_CODES_H
+# define KEY_CODES_H
+
+# define UP_KEY 13
+# define LEFT_KEY 0
+# define RIGHT_KEY 2
+# define DOWN_KEY 1
+# define ESC_KEY 53
+
+#endif
--- srcs/main_game.c
+++ srcs/main_game.c
@@ -11,13 +11,68 @@
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include "key_codes.h"
+
+void ft_putchar(int c)
+{
+	write(1,&c,1);
+}
+
+static void	recurs(long n, int *index)
+{
+	(*index)++;
+	if (n < 10)
+		ft_putchar(n + '0');
+	else
+	{
+		recurs(n / 10, index);
+		ft_putchar((n % 10) + '0');
+	}
+}
+
+int	ft_put_u(unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	recurs(n, &i);
+	return (i);
+}
+
+
+int f(int key, t_game *param)
+{
+	static int color=255;
+	static int x=100;
+	static int y=100;
+	ft_put_u(key);
+	if(key == UP_KEY)
+		y-=10;
+	else if(key == DOWN_KEY)
+		y+=10;
+	else if(key == LEFT_KEY)
+		x-=10;
+	else if (key == RIGHT_KEY)
+		x+=10;
+	else if(key == ESC_KEY)
+	{
+		mlx_destroy_window(param->)
+	}
+	
+	mlx_pixel_put(param->mlx,param->win,x,y,color);
+
+
+	color+=0x0000FF;
+	return 0;
+}
 
 void main_game(t_map map)
 {
 	t_game game;
     game.mlx=mlx_init() ;
 	game.win=mlx_new_window(game.mlx,map.x * 100,map.y * 100,"bunda");
-
+	mlx_key_hook(game.win, f, &game);
+	// ft_put_u(i);
 	mlx_loop(game.mlx);
 
 }
