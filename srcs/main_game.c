2d60f33e1bb7daec3f0445888078f986d5ee3943
Ahmed El Maazouzi <ael-maaz@c1r7p1.1337.ma>
2024-02-28 22:53:55 +0100

    added a MACRO

--- srcs/main_game.c
+++ srcs/main_game.c
@@ -13,6 +13,8 @@
 #include "so_long.h"
 #include "key_codes.h"
 
+#define MLX_PUT mlx_put_image_to_window
+
 void	load_textures(t_txts *x, t_game g)
 {
 	x->w.img = mlx_xpm_file_to_image(g.mlx, "./textures/W.xpm", 
@@ -48,55 +50,55 @@ void	draw_map(t_game g, t_map map, int i, int j)
 		while (map.lines[i][++j])
 		{
 			if (map.lines[i][j] == '1')
-				mlx_put_image_to_window(g.mlx, g.win, 
-					img.w.img, j * 64, i * 64);
+				MLX_PUT(g.mlx, g.win, img.w.img, j * 64, i * 64);
 			else if (map.lines[i][j] == 'C')
 			{
-				mlx_put_image_to_window(g.mlx, g.win,
-					img.f.img, j * 64, i * 64);
-				mlx_put_image_to_window(g.mlx, g.win,
-					img.c.img, j * 64, i * 64);
+				MLX_PUT(g.mlx, g.win, img.f.img, j * 64, i * 64);
+				MLX_PUT(g.mlx, g.win, img.c.img, j * 64, i * 64);
 			}
 			else
-				mlx_put_image_to_window(g.mlx, g.win,
-					img.f.img, j * 64, i * 64);
+				MLX_PUT(g.mlx, g.win, img.f.img, j * 64, i * 64);
 		}
 	}
-	mlx_put_image_to_window(g.mlx, g.win, img.p.img, g.p.x, g.p.y);
+	MLX_PUT(g.mlx, g.win, img.p.img, g.p.x, g.p.y);
 	if (g.p.c_col == g.map.coins)
-		mlx_put_image_to_window(g.mlx, g.win, img.e_o.img, map.exit_x * 64, map.exit_y * 64);
+		MLX_PUT(g.mlx, g.win, img.e_o.img, map.exit_x * 64, map.exit_y * 64);
 	else
-		mlx_put_image_to_window(g.mlx, g.win, img.e_c.img, map.exit_x * 64, map.exit_y * 64);
-}
-
-int f(int key, t_game *param)
-{
-	if(key == UP_KEY && param->map.lines[(param->p.y-64)/64][param->p.x/64]!='1')
-		param->p.y-=64;
-	else if(key == DOWN_KEY  && param->map.lines[(param->p.y+64)/64][param->p.x/64]!='1')
-		param->p.y+=64;
-	else if(key == LEFT_KEY && param->map.lines[param->p.y/64][(param->p.x-64)/64]!='1')
-		param->p.x-=64;
-	else if (key == RIGHT_KEY && param->map.lines[param->p.y/64][(param->p.x+64)/64]!='1')
-		param->p.x+=64;
-	else if(key == ESC_KEY )
-	{
-		mlx_destroy_window(param->mlx,param->win);
-		exit(1);
-	}
-	if(param->map.lines[param->p.y/64][param->p.x/64]=='C')
-	{
-		param->p.c_col++;
-		param->map.lines[param->p.y/64][param->p.x/64]='0';
-	}
-	draw_map(*param, param->map, -1, -1);
-	return (0);
+		MLX_PUT(g.mlx, g.win, img.e_c.img, map.exit_x * 64, map.exit_y * 64);
 }
 
 int	quit(t_game *param)
 {
 	mlx_destroy_window(param->mlx, param->win);
 	exit(1);
+}
+
+int	f(int key, t_game *pm)
+{
+	if (key == UP_KEY && 
+		pm->map.lines[(pm->p.y - 64) / 64][pm->p.x / 64] != '1')
+		pm->p.y -= 64;
+	else if (key == DOWN_KEY
+		&& pm->map.lines[(pm->p.y + 64) / 64][pm->p.x / 64] != '1')
+		pm->p.y += 64;
+	else if (key == LEFT_KEY
+		&& pm->map.lines[pm->p.y / 64][(pm->p.x - 64) / 64] != '1')
+		pm->p.x -= 64;
+	else if (key == RIGHT_KEY
+		&& pm->map.lines[pm->p.y / 64][(pm->p.x + 64) / 64] != '1')
+		pm->p.x += 64;
+	else if (key == ESC_KEY)
+		quit(pm);
+	if (pm->map.lines[pm->p.y / 64][pm->p.x / 64] == 'C')
+	{
+		pm->p.c_col++;
+		pm->map.lines[pm->p.y / 64][pm->p.x / 64] = '0';
+	}
+	if (pm->p.c_col == pm->map.coins 
+		&& pm->p.x == pm->map.exit_x * 64 && pm->p.y == pm->map.exit_y * 64)
+		quit(pm);
+	draw_map(*pm, pm->map, -1, -1);
+	return (0);
 }
 
 void	main_game(t_map map)
