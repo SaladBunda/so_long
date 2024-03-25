09d743b997e24dedb73cf413c43d5f89d3ac7b90
SaladBunda <khahmaaz@gmail.com>
2024-03-25 23:54:43 +0000

    havent fixed the pointer being freed was not allocated

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ael-maaz <ael-maaz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 22:47:02 by ael-maaz          #+#    #+#             */
-/*   Updated: 2024/03/25 23:52:06 by ael-maaz         ###   ########.fr       */
+/*   Updated: 2024/03/25 23:54:14 by ael-maaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,26 +54,29 @@ void freeing_map(t_map *map)
 
 void	print_errors(int param, t_map *map)
 {
-	
-	if(param == 2)
-		write(2,"Invalid lines length\n",21);
-	else if(param == 3)
+	if(param == 6)
 	{
-		if(fill_pixels(map,-1, -1) == 0)
-			write(2, "Invalid characters in map\n",26);
-		else if(fill_pixels(map,-1, -1) == 10)
-			write(2, "Invalid number of Player/Exit positions\n",25);
-		else if(fill_pixels(map,-1, -1) == 11)
-			write(2, "The map must contain at least 1 coin\n",37);
-			
+		if(param == 2)
+			write(2,"Invalid lines length\n",21);
+		else if(param == 3)
+		{
+			if(fill_pixels(map,-1, -1) == 0)
+				write(2, "Invalid characters in map\n",26);
+			else if(fill_pixels(map,-1, -1) == 10)
+				write(2, "Invalid number of Player/Exit positions\n",25);
+			else if(fill_pixels(map,-1, -1) == 11)
+				write(2, "The map must contain at least 1 coin\n",37);
+				
+		}
+		else if(param == 4)
+			write(2,"Invalid borders\n",16);
+		else if(param == 5)
+			write(2,"Invalid path\n",13);
+		else if(param == 0)
+			write(2,"Error while creating game\n",26);
+		freeing_map(map);
 	}
-	else if(param == 4)
-		write(2,"Invalid borders\n",16);
-	else if(param == 5)
-		write(2,"Invalid path\n",13);
-	else if(param == 0)
-		write(2,"Error while creating game\n",26);
-	freeing_map(map);
+	write(2, "Inexsitant map file\n",20);
 }
 
 int	main(int ac, char **av)
